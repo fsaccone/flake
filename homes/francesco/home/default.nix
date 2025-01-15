f213903a7211ab0a8a4bf77d88d490316c219a48
Francesco Saccone <francesco@francescosaccone.com>
2025-01-15 15:20:47 +0100

    Enable neomutt module in francesco's home

--- homes/francesco/home/default.nix
+++ homes/francesco/home/default.nix
@@ -10,6 +10,7 @@
     gpg.enable = true;
     mediaViewers.enable = true;
     moneroWallet.enable = true;
+    neomutt.enable = true;
     neovim.enable = true;
     sway.enable = true;
     syncthing.enable = true;
