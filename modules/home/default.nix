3bab60b50e5a4c308b80a0f38d4a7655e99d20ef
Francesco Saccone <francesco@francescosaccone.com>
2025-01-15 15:20:27 +0100

    Add neomutt module to index

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -11,6 +11,7 @@
     ./gpg
     ./mediaViewers
     ./moneroWallet
+    ./neomutt
     ./neovim
     ./sway
     ./syncthing
@@ -22,6 +23,7 @@
     gpg.enable = lib.mkDefault false;
     mediaViewers.enable = lib.mkDefault false;
     moneroWallet.enable = lib.mkDefault false;
+    neomutt.enable = lib.mkDefault false;
     neovim.enable = lib.mkDefault false;
     sway.enable = lib.mkDefault false;
     syncthing.enable = lib.mkDefault false;
