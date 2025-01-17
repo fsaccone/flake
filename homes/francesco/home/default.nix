8ce9491a0fba2a16f5c67951f6cbfbcedd223449
Francesco Saccone <francesco@francescosaccone.com>
2025-01-17 11:43:10 +0100

    Remove neomutt, add aerc and apply gnupg module changes

--- homes/francesco/home/default.nix
+++ homes/francesco/home/default.nix
@@ -5,12 +5,19 @@
 }:
 {
   modules = {
+    aerc.enable = true;
     firefox.enable = true;
     git.enable = true;
-    gpg.enable = true;
+    gpg = {
+      enable = true;
+      primaryKey = {
+        fingerprint = "42616543258F1BD93E84F0DB63A0ED9A00042E8C";
+        url = "https://keys.openpgp.org/vks/v1/by-fingerprint/42616543258F1BD93E84F0DB63A0ED9A00042E8C";
+        sha256 = "a833ae43e62b9b9b61e274e3749a4f870f46b7c99bf885e7b85fe4bedb244648";
+      };
+    };
     mediaViewers.enable = true;
     moneroWallet.enable = true;
-    neomutt.enable = true;
     neovim.enable = true;
     sway.enable = true;
     syncthing.enable = true;
@@ -24,6 +31,5 @@
     musescore
     qjackctl
     rsync
-    thunderbird
   ];
 }
--- modules/home/default.nix
+++ modules/home/default.nix
@@ -6,24 +6,24 @@
 }:
 {
   imports = [
+    ./aerc
     ./firefox
     ./git
     ./gpg
     ./mediaViewers
     ./moneroWallet
-    ./neomutt
     ./neovim
     ./sway
     ./syncthing
   ];
 
   config.modules = {
+    aerc.enable = lib.mkDefault false;
     firefox.enable = lib.mkDefault false;
     git.enable = lib.mkDefault false;
     gpg.enable = lib.mkDefault false;
     mediaViewers.enable = lib.mkDefault false;
     moneroWallet.enable = lib.mkDefault false;
-    neomutt.enable = lib.mkDefault false;
     neovim.enable = lib.mkDefault false;
     sway.enable = lib.mkDefault false;
     syncthing.enable = lib.mkDefault false;
