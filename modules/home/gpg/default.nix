e04a90aef54e9d3cc39380c6ed4932641593c748
Francesco Saccone <francesco@francescosaccone.com>
2025-01-17 11:40:19 +0100

    Make GnuPG home module more customisable

--- modules/home/gpg/default.nix
+++ modules/home/gpg/default.nix
@@ -7,7 +7,29 @@
 }:
 {
   options.modules = {
-    gpg.enable = lib.mkEnableOption "Enables GnuPG";
+    gpg = {
+      enable = lib.mkEnableOption "Enables GnuPG";
+      primaryKey = {
+        fingerprint = lib.mkOption {
+          type = lib.types.uniq lib.types.str;
+          description = ''
+            The fingerprint of the primary key.
+          '';
+        };
+        url = lib.mkOption {
+          type = lib.types.uniq lib.types.str;
+          description = ''
+            The URL to the primary key file.
+          '';
+        };
+        sha256 = lib.mkOption {
+          type = lib.types.uniq lib.types.str;
+          description = ''
+            The SHA256 of the primary key file.
+          '';
+        };
+      };
+    };
   };
 
   config = lib.mkIf config.modules.gpg.enable {
@@ -15,19 +37,26 @@
       enable = true;
       package = pkgs.gnupg;
 
+      mutableKeys = false;
+      mutableTrust = false;
       settings = {
         "pinentry-mode" = "loopback";
       };
+
+      publicKeys = [
+        {
+          source = builtins.fetchurl {
+            inherit (config.modules.gpg.primaryKey) url sha256;
+          };
+          trust = "ultimate";
+        }
+      ];
     };
 
     services.gpg-agent = {
       enable = true;
       enableSshSupport = true;
       pinentryPackage = pkgs.pinentry-tty;
-      extraConfig = ''
-        allow-loopback-pinentry
-        no-grab
-      '';
     };
   };
 }
