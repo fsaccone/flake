5f2487e6baa28b163d84a152c670aec4d5eee5b6
Francesco Saccone <francesco@francescosaccone.com>
2025-01-23 14:00:27 +0100

    Remove old Firefox code from Librewolf module

--- modules/home/librewolf/default.nix
+++ modules/home/librewolf/default.nix
@@ -40,67 +40,6 @@
         "browser.search.order.1" = engine.name;
         "browser.startup.homepage" = engine.url;
       };
-
-      profiles = {
-        default = {
-          id = 0;
-          name = "default";
-          isDefault = true;
-          extraConfig = builtins.readFile (inputs.arkenfox-userjs + "/user.js");
-          settings = {
-            "browser.bookmarks.addedImportButton" = "false";
-            "browser.toolbars.bookmarks.visibility" = "always";
-          };
-          search = {
-            force = true;
-            privateDefault = "Searx";
-            default = "Searx";
-            order = [ "Searx" ];
-            engines = {
-              "NixOS Packages" = {
-                urls = [
-                  {
-                    template = "https://search.nixos.org/packages";
-                    params = [
-                      {
-                        name = "channel";
-                        value = "unstable";
-                      }
-                      {
-                        name = "type";
-                        value = "packages";
-                      }
-                      {
-                        name = "query";
-                        value = "{searchTerms}";
-                      }
-                    ];
-                  }
-                ];
-                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = [ "@np" ];
-              };
-              "Searx" = {
-                urls = [
-                  {
-                    template = "http://localhost:8888";
-                    params = [
-                      {
-                        name = "q";
-                        value = "{searchTerms}";
-                      }
-                    ];
-                  }
-                ];
-                definedAliases = [ "@searx" ];
-              };
-            };
-          };
-          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-            ublock-origin
-          ];
-        };
-      };
     };
   };
 }
