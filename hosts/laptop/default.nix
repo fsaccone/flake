93110dc448e85744727a69fa6e3d12c682a0352a
Francesco Saccone <francesco@francescosaccone.com>
2025-03-18 18:59:13 +0100

    feat: remove @audio and @realtime login limits settings
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -43,31 +43,4 @@
       editor = false;
     };
   };
-
-  security.pam.loginLimits = [
-    {
-      domain = "@realtime";
-      type = "hard";
-      item = "rtprio";
-      value = 20;
-    }
-    {
-      domain = "@realtime";
-      type = "soft";
-      item = "rtprio";
-      value = 10;
-    }
-    {
-      domain = "@audio";
-      type = "-";
-      item = "rtprio";
-      value = 95;
-    }
-    {
-      domain = "@audio";
-      type = "-";
-      item = "memlock";
-      value = "unlimited";
-    }
-  ];
 }
