7697e0963c7e408877897499b1e67013339385c6
Francesco Saccone <francesco@francescosaccone.com>
2025-03-24 20:28:50 +0100

    feat: add /git symlink to darkhttpd
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -47,6 +47,7 @@ rec {
         "index.gmi" = "/tmp/site/gemini/index.gmi";
         "blog" = "/tmp/site/gemini/blog";
         "code" = "/tmp/site/gemini/code";
+        "git" = "/tmp/stagit";
         "public" = "${inputs.site}/public";
         "robots.txt" = "${inputs.site}/robots.txt";
         "atom.xml" = "/tmp/site/gemini/atom.xml";
