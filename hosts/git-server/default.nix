e055aad1537fc550d9a14a8fa746ffb95ec3a5d0
Francesco Saccone <francesco@francescosaccone.com>
2025-03-28 18:01:57 +0100

    feat: add 'hermes' repository
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- hosts/git-server/default.nix
+++ hosts/git-server/default.nix
@@ -91,6 +91,9 @@ in
           flake = {
             description = "Francesco Saccone's Nix flake.";
           };
+          hermes = {
+            description = "HTTP GET/HEAD-only web server for static content.";
+          };
           password-store = {
             description = "Francesco Saccone's password store.";
           };
