2fa678e2919c357173d1541b79d64fce7fb372c3
Francesco Saccone <francesco@francescosaccone.com>
2025-04-06 16:59:31 +0200

    feat: define 'background' color
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- modules/home/sway/colors.nix
+++ modules/home/sway/colors.nix
@@ -1,4 +1,8 @@
 {
+  # This is the color to use for the status bar, it should be continous with
+  # the background image.
+  background = "#00112b";
+
   black = "#000000";
   red = "#da8b8b";
   white = "#ffffff";
