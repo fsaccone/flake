6e07cd60f00425850049213a7122990237fbe3d3
Francesco Saccone <francesco@francescosaccone.com>
2025-04-17 16:08:27 +0200

    Revert "feat: remove @audio and @realtime login limits settings"
    This reverts commit 93110dc448e85744727a69fa6e3d12c682a0352a.

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -86,4 +86,31 @@
         home.stateVersion = "25.05";
       };
   };
+
+  security.pam.loginLimits = [
+    {
+      domain = "@realtime";
+      type = "hard";
+      item = "rtprio";
+      value = 20;
+    }
+    {
+      domain = "@realtime";
+      type = "soft";
+      item = "rtprio";
+      value = 10;
+    }
+    {
+      domain = "@audio";
+      type = "-";
+      item = "rtprio";
+      value = 95;
+    }
+    {
+      domain = "@audio";
+      type = "-";
+      item = "memlock";
+      value = "unlimited";
+    }
+  ];
 }
