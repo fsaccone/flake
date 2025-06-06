c86acff3a7be5a2e5ea9b087f9c9b5ef0e33ee22
Francesco Saccone <francesco@francescosaccone.com>
2025-06-06 17:26:56 +0200

    refactor: rewrite stagit scripts into a single one in default.nix
    It was such a mess. Also, it does not run Stagit as a prescript
    to the web server anymore.
    
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- hosts/git-server/default.nix
+++ hosts/git-server/default.nix
@@ -10,12 +10,55 @@ let
   rootDomain = import "${mainServer}/domain.nix";
   gitDomain = "git.${rootDomain}";
 
-  scripts = import ./scripts.nix { inherit config pkgs inputs; };
+  generateStagitRepository =
+    let
+      inherit (config.fs.services) merecat git;
+    in
+    name:
+    pkgs.writeShellScript "generate-stagit" ''
+      set -e
 
-  stagit = {
-    destDir = config.fs.services.merecat.directory;
-    reposDir = config.fs.services.git.directory;
-  };
+      # Create index.html
+      ${pkgs.stagit}/bin/stagit-index \
+        ${git.directory}/*/ > ${merecat.directory}/index.html
+
+      # Copy favicon.png, logo.png, style.css from site repository
+      ${pkgs.sbase}/bin/cp \
+        ${inputs.site}/public/icon/256.png \
+        ${merecat.directory}/favicon.png
+
+      ${pkgs.sbase}/bin/cp \
+        ${inputs.site}/public/icon/32.png \
+        ${merecat.directory}/logo.png
+
+      ${pkgs.sbase}/bin/cp \
+        ${inputs.site}/public/stagit.css \
+        ${merecat.directory}/style.css
+
+      ${pkgs.sbase}/bin/echo \
+        "Stagit index file generated: <www>/index.html".
+
+      # Create repository pages
+      ${pkgs.sbase}/bin/mkdir -p ${merecat.directory}/${name}
+
+      cd ${merecat.directory}/${name}
+
+      ${pkgs.stagit}/bin/stagit \
+        -l 100 \
+        -u https://${gitDomain} \
+        ${git.directory}/${name}
+
+      # Make log.html the default page
+      ${pkgs.sbase}/bin/ln -sf log.html index.html
+
+      # Symlink the static files from the index page
+      ${pkgs.sbase}/bin/ln -sf ../style.css style.css
+      ${pkgs.sbase}/bin/ln -sf ../favicon favicon
+      ${pkgs.sbase}/bin/ln -sf ../logo.png logo.png
+
+      ${pkgs.sbase}/bin/echo \
+        "Stagit page generated for ${name}: <www>/${name}."
+    '';
 in
 {
   imports = [ ./disk-config.nix ];
@@ -53,15 +96,35 @@ in
                 url = "https://${gitDomain}/${name}";
               };
               hooks.postReceive =
-                builtins.concatStringsSep "\n" [
-                  (scripts.stagitPostReceive {
-                    inherit (stagit) destDir reposDir;
-                    inherit name;
-                    httpBaseUrl = "https://${gitDomain}";
-                  })
-                  "git update-server-info" # Dumb HTTP protocol
-                ]
-                |> pkgs.writeShellScript "post-receive";
+                let
+                  inherit (config.fs.services) merecat git;
+                in
+                pkgs.writeShellScript "post-receive" ''
+                  set -e
+
+                  # Define is_force=1 if 'git push -f' was used
+                  null_ref="0000000000000000000000000000000000000000"
+                  is_force=0
+                  while read -r old new red; do
+                    ${pkgs.sbase}/bin/test "$old" = $null_ref && continue
+                    ${pkgs.sbase}/bin/test "$new" = $null_ref && continue
+
+                    has_revs=$(${pkgs.git}/bin/git rev-list "$old" "^$new" | \
+                               ${pkgs.sbase}/bin/sed 1q)
+
+                    if ${pkgs.sbase}/bin/test -n "$has_revs"; then
+                      is_force=1
+                      break
+                    fi
+                  done
+
+                  # If is_force is 1, delete HTML commits
+                  if ${pkgs.sbase}/bin/test $is_force = 1; then
+                    ${pkgs.sbase}/bin/rm -rf ${merecat.directory}/${name}/commit
+                  fi
+
+                  ${generateStagitRepository name}
+                '';
             }
           );
         daemon = {
@@ -74,21 +137,13 @@ in
         preStart = {
           scripts =
             let
-              stagitCreate = scripts.stagitCreate {
-                inherit (stagit) destDir reposDir;
-                httpBaseUrl = "https://${gitDomain}";
-              };
-
               copyRepositories = pkgs.writeShellScript "copy-repositories" ''
                 ${pkgs.sbase}/bin/cp -fRL \
                   ${config.fs.services.git.directory}/* \
                   ${config.fs.services.merecat.directory}
               '';
             in
-            [
-              stagitCreate
-              copyRepositories
-            ];
+            [ copyRepositories ];
         };
         acme = {
           enable = true;
