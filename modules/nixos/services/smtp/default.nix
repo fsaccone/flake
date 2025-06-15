239725ede8cfec55b71c46760fddf9b9287c2ebf
Francesco Saccone <francesco@francescosaccone.com>
2025-06-15 17:08:36 +0200

    refactor: use smtpd -f flag instead of /etc/mail/smtpd.conf
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- modules/nixos/services/smtp/default.nix
+++ modules/nixos/services/smtp/default.nix
@@ -25,33 +25,33 @@
   };
 
   config = lib.mkIf config.fs.services.smtp.enable {
-    environment.etc."mail/smtpd.conf".text =
-      let
-        inherit (config.fs.services.smtp) tls;
-      in
-      ''
-        pki tls cert "${tls.certificate}"
-        pki tls key "${tls.key}"
-
-        listen on all smtps verify pki tls
-        listen on all tls pki tls
-      '';
-
     systemd = {
       services = {
         smtp = {
           enable = true;
           wantedBy = [ "multi-user.target" ];
           after = [ "network.target" ];
-          serviceConfig = {
-            User = "root";
-            Group = "root";
-            Restart = "on-failure";
-            Type = "simple";
-            ExecStart = pkgs.writeShellScript "smtp" ''
-              ${pkgs.opensmtpd}/bin/smtpd -d
-            '';
-          };
+          serviceConfig =
+            let
+              inherit (config.fs.services.smtp) tls;
+
+              configuration = builtins.toFile "smtpd.conf" ''
+                pki tls cert "${tls.certificate}"
+                pki tls key "${tls.key}"
+
+                listen on all smtps verify pki tls
+                listen on all tls pki tls
+              '';
+            in
+            {
+              User = "root";
+              Group = "root";
+              Restart = "on-failure";
+              Type = "simple";
+              ExecStart = pkgs.writeShellScript "smtp" ''
+                ${pkgs.opensmtpd}/bin/smtpd -df ${configuration}
+              '';
+            };
         };
       };
     };
