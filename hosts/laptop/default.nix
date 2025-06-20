403392238fd190906b0ebf7494d4c4b9c6dcafb7
Francesco Saccone <francesco@francescosaccone.com>
2025-06-20 13:13:22 +0200

    fix: move fs.programs.monero configuration to fs.services.monero
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -7,20 +7,19 @@
 }:
 {
   fs = {
-    programs.monero = {
-      enable = true;
-      mining = {
-        enable = true;
-        address = builtins.concatStringsSep "" [
-          "47y5LAtYdpZ4GAE7CMx1soEHjUKzpVQFYM5Pv836FcsZd6k3TFcdvHMAHDpwZgnx"
-          "4DdG2zkZkSewLgguU23FYJP7HacSVcx"
-        ];
-      };
-    };
-
     services = {
       ly = {
         enable = true;
+      };
+      monero = {
+        enable = true;
+        mining = {
+          enable = true;
+          address = builtins.concatStringsSep "" [
+            "47y5LAtYdpZ4GAE7CMx1soEHjUKzpVQFYM5Pv836FcsZd6k3TFcdvHMAHDpwZgnx"
+            "4DdG2zkZkSewLgguU23FYJP7HacSVcx"
+          ];
+        };
       };
       sway = {
         enable = true;
