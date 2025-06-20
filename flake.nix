06616089f8c885f97470a8396ca080781a063d8a
Francesco Saccone <francesco@francescosaccone.com>
2025-06-20 12:47:39 +0200

    build: update nixpkgs and home-manager to 25.05
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/release-24.11";
+      url = "github:nixos/nixpkgs/release-25.05";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     treefmt-nix = {
