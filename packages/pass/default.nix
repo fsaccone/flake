764f0f9fd28fc3e416c2b9f0c62608e8d5404ae8
Francesco Saccone <francesco@francescosaccone.com>
2025-06-26 15:58:24 +0200

    fix: update pass and define PASS_PROGRAM and SOURCE_DIRECTORY
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- packages/pass/default.nix
+++ packages/pass/default.nix
@@ -5,23 +5,34 @@
   pass,
   xclip,
 }:
+let
+  extendedPass = pass.withExtensions (exts: [ exts.pass-otp ]);
+in
 stdenv.mkDerivation rec {
   name = "pass";
 
   src = fetchgit {
     url = "git://git.francescosaccone.com/pass";
-    rev = "a4307edc2cfff354d448d4a7df538aee0c3e55d7";
-    sha256 = "sha256-2AocSyHfjfinloder1hb254wS3bly6MNtHMzShxTenA=";
+    rev = "c6afc30899ea2f4ff4e07d9bd7d06e24a497ed62";
+    sha256 = "sha256-aqlGvXTIIqwsuRQ9UEEM70Kf/3TAAg8h+etzocgZRHw=";
   };
 
   buildInputs = [
-    (pass.withExtensions (exts: [ exts.pass-otp ]))
+    extendedPass
     xclip
   ];
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp $src/pass.sh $out/bin/pass
+    mkdir -p $out/{bin,share/store}
+
+    {
+      echo "#!/bin/sh"
+      echo "export PASS_PROGRAM=${extendedPass}/bin/pass"
+      echo "export SOURCE_DIRECTORY=$out/share/store"
+      cat $src/pass.sh
+    } > $out/bin/pass
+
+    cp -r $src/store/* $out/share/store/
     chmod +x $out/bin/pass
   '';
 
