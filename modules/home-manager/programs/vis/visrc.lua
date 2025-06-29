a2b62e6f61da2770dad63de4c5c2cafde40b9e82
Francesco Saccone <francesco@francescosaccone.com>
2025-06-29 13:37:17 +0200

    feat: use 2-spaces in .nix files and 8-tab in everything else
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- modules/home-manager/programs/vis/visrc.lua
+++ modules/home-manager/programs/vis/visrc.lua
@@ -1,11 +1,22 @@
 require('vis')
 
+function endsWith(str, suffix)
+  return str:sub(-#suffix) == suffix
+end
+
+
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
   vis:command('set autoindent on')
   vis:command('set colorcolumn 80')
-  vis:command('set expandtab on')
   vis:command('set number')
   vis:command('set show-spaces on')
   vis:command('set show-tabs on')
-  vis:command('set tabwidth 2')
+
+  if endsWith(win.file.name, ".nix") then
+    vis:command('set expandtab on')
+    vis:command('set tabwidth 2')
+  else
+    vis:command('set expandtab off')
+    vis:command('set tabwidth 8')
+  end
 end)
