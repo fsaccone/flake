874732ffb92c6038f14340cdc7067100affbb578
Francesco Saccone <francesco@francescosaccone.com>
2025-10-24 16:46:20 +0200

    fix(hosts/hades): update disk configuration

--- hosts/hades/disk-config.nix
+++ hosts/hades/disk-config.nix
@@ -1,8 +1,8 @@
 { config, lib, ... }:
 {
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/sda";
+    disk.main = {
+      device = "/dev/sda";
       type = "disk";
       content = {
         type = "gpt";
@@ -20,30 +20,16 @@
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
             };
           };
           root = {
             name = "root";
             size = "100%";
             content = {
-              type = "lvm_pv";
-              vg = "pool";
-            };
-          };
-        };
-      };
-    };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
-              mountOptions = [ "defaults" ];
             };
           };
         };
