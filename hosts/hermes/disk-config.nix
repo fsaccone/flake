98ee90b7d2d3b62b58e9a944ece09827a26db06f
Francesco Saccone <francesco@francescosaccone.com>
2025-10-25 14:30:11 +0200

    feat(hosts/hermes): update disk config

--- hosts/hermes/disk-config.nix
+++ hosts/hermes/disk-config.nix
@@ -1,17 +1,12 @@
 { config, lib, ... }:
 {
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/sda";
+    disk.main = {
+      device = "/dev/sda";
       type = "disk";
       content = {
         type = "gpt";
         partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
           esp = {
             name = "ESP";
             size = "500M";
@@ -20,30 +15,16 @@
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
             };
           };
           root = {
             name = "root";
             size = "100%";
             content = {
-              type = "lvm_pv";
-              vg = "pool";
-            };
-          };
-        };
-      };
-    };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
-              mountOptions = [ "defaults" ];
             };
           };
         };
